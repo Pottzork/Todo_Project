b086448e2a32c5b6112fa286dff64f246c76c114
Daniel Söderberg <dansod85@gmail.com>
2020-09-10 16:08:36 +0200

    databasTry

---
+++ DatabasTodo/DataLibrary/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary
+{
+    public class Staff
+    {
+        public int StaffId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Operator { get; set; }
+    }
+}
---
+++ DatabasTodo/DataLibrary/WorkOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary
+{
+    public class WorkOrder
+    {
+        public int WorkOrderId { get; set; }
+        public DateTime WorkOrderDate { get; set; }
+        public string WorkOrderDescription { get; set; }
+        public string WorkOrderAddress { get; set; }
+        public string WorkOrderPhoneNumber { get; set; }
+        public string WorkOrderContactName { get; set; }
+    }
+}
