c9594b1daf02753d7dc1d03052b73218dbe21061
Daniel Söderberg <dansod85@gmail.com>
2020-09-10 16:22:55 +0200

    models and more

---
+++ DatabasTodo/WebApiTodo/Data/TheDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiTodo.Data
+{
+    public class TheDbContext : TheDbContext
+    {
+
+        public TheDbContext(DbContextOptions<TheDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+
+
+
+
+
+
+    }
+}
