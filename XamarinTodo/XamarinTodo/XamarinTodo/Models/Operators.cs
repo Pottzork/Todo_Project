8e98e9040d4af6baf7d8a5dee2fc211c93d83bfb
DanielSod <dansod85@gmail.com>
2020-09-15 09:30:22 +0200

    Models done

--- XamarinTodo/XamarinTodo/XamarinTodo/Models/Operators.cs
+++ XamarinTodo/XamarinTodo/XamarinTodo/Models/Operators.cs
@@ -9,6 +9,11 @@ namespace XamarinTodo.Models
         public int OpId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Location { get; set; }
 
 
     }
