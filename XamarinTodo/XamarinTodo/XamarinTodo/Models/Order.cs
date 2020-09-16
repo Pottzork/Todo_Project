0c1b9e33b3f817de099394483395e1ab248a9ed7
AndyCarlsson <andreas95carlsson@gmail.com>
2020-09-16 14:54:17 +0200

    Updated orders Model

--- XamarinTodo/XamarinTodo/XamarinTodo/Models/Order.cs
+++ XamarinTodo/XamarinTodo/XamarinTodo/Models/Order.cs
@@ -3,24 +3,19 @@ using System.Collections.Generic;
 using System.Text;
 using XamarinTodo.Pages;
 
-
 namespace XamarinTodo.Models
 {
     public class Orders
     {
         public int OrderId { get; set; }
         public string OrderDescription { get; set; }
-        
-        public string OrderResponse { get; set; }
         public bool OrderAccept { get; set; }
-        public DateTime OrderEstTime { get; set; }
         public DateTime OrderStart { get; set; }
         public DateTime OrderEnd { get; set; }
         public bool OrderComplete { get; set; }
         public float OrderPrice { get; set; }
-
-        public int CusId { get; set; }
-
-
+        public string OrderInfo { get; set; }
+        public int CustomerCusId { get; set; }
+        public int OperatorID { get; set; }
     }
 }
