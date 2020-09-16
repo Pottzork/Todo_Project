64f144427bbe4b69e1e303aa1d6274fce0064e39
Daniel Söderberg <dansod85@gmail.com>
2020-09-16 15:08:49 +0200

    lkala

---
+++ NewBlazorTodo/NewBlazorTodo/Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NewBlazorTodo.Shared.Models;
+
+namespace NewBlazorTodo.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Developer> Developers { get; set; }
+
+        public DbSet<Admins> Admins { get; set; }
+        public DbSet<Customers> Customers { get; set; }
+        public DbSet<Operators> Operators { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+    }
+}
