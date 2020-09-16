caa7e2d1f5acdf78ddd9320a66f162a020d59300
DanielSod <dansod85@gmail.com>
2020-09-16 15:20:00 +0200

    Setup

---
+++ MockXamTodo/MockXamTodo/MockXamTodo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using MockXamTodo.ViewModels;
+
+namespace MockXamTodo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
