b3b5df9d0d00124f8417d74bcc1f7c17c652db45
Mattis <Mattis.vainikainen@gmail.com>
2020-09-21 12:33:43 +0200

    updates

---
+++ WebApiTodo/DataAccess/Services/IOrderOverViewService.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Services
+{
+    public interface IOrderOverViewService
+    {
+        Task<IEnumerable<OrderOverView>> GetOrderOverViews();
+        Task<OrderOverView> GetOrderOverView(int id);
+        Task<IEnumerable<OrderOverView>> GetListOfOrderOverViews(int id);
+    }
+}
