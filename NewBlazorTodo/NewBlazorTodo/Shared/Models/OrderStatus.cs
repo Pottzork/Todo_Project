9a24c4501f8590ad621edc9c8aadb2e3fd43fe08
Mattis <Mattis.vainikainen@gmail.com>
2020-09-21 02:53:20 +0200

    La till så man kunde tilldela en annan operatör till en nekad prder

--- NewBlazorTodo/NewBlazorTodo/Shared/Models/OrderStatus.cs
+++ NewBlazorTodo/NewBlazorTodo/Shared/Models/OrderStatus.cs
@@ -6,11 +6,11 @@ namespace NewBlazorTodo.Shared.Models
 {
     public enum OrderStatus
     {
-        SCHEMALAGD, //Operatör har lagt order i sitt schema.
-        ACCEPTERAD, //Operatör har tagit emot och skall börja
-        NEKAD, //Operatör har nekat arbetsorder
-        EJ_ACCEPTERAD, //Operatör har inte "sett" arbetsordern
-        KLAR, // Operatör har gjort klart order
-        BETALD // Admin har betalat och order visas endast i history tabellen
+        SCHEMALAGD, // (0) Operatör har lagt order i sitt schema. 
+        ACCEPTERAD, // (1) Operatör har tagit emot och skall börja 
+        NEKAD, // (2) Operatör har nekat arbetsorder
+        EJ_ACCEPTERAD, // (3) Operatör har inte "sett" arbetsordern
+        KLAR, // (4) Operatör har gjort klart order
+        BETALD // (5) Admin har betalat och order visas endast i history tabellen
     }
 }
