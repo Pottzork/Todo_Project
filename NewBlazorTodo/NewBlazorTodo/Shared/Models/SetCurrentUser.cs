c622cdc33852fa200745407b482c0f138593b4c3
Mattis <Mattis.vainikainen@gmail.com>
2020-09-21 22:02:47 +0200

    La till setcurrentuser

---
+++ NewBlazorTodo/NewBlazorTodo/Shared/Models/SetCurrentUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewBlazorTodo.Shared.Models
+{
+    public class SetCurrentUser
+    {
+        public static int Id { get; set; }      
+
+        public static string UserName { get; set; }
+        public static string Password { get; set; }
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+        public static string Phone { get; set; }
+    }
+}
