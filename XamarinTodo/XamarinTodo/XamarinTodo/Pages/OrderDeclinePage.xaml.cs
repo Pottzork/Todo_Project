0360a91641fb6d27dac1c1aa6cb4394499930610
DanielSod <dansod85@gmail.com>
2020-09-22 09:43:25 +0200

    try app

--- XamarinTodo/XamarinTodo/XamarinTodo/Pages/OrderDeclinePage.xaml.cs
+++ XamarinTodo/XamarinTodo/XamarinTodo/Pages/OrderDeclinePage.xaml.cs
@@ -28,42 +28,32 @@ namespace XamarinTodo.Pages
 
         private async void SendReason_Clicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Alert", "Är du säker på att du vill avböja order?", "Nej.", "Ja!");
+            var answer = await DisplayAlert("Alert", "Är du säker på att du vill avböja order?", "Nej.", "Ja!");
 
-            Order.OrderStatus = OrderStatus.EJ_ACCEPTERAD;
-
-            DeclineOrder();
-
-            await Navigation.PushModalAsync(new NavigationPage(new OrderOverviewPage()));
+            if (answer ==true)
+            {
+                DeclineOrder();
+                await Navigation.PushModalAsync(new NavigationPage(new OrderOverviewPage()));
+            }
+            
 
         }
 
 
         public void DeclineOrder()
         {
+            Order.OrderStatus = OrderStatus.EJ_ACCEPTERAD;
             DateTime timeLogNow = DateTime.Now;
-            //StringBuilder orderDeclineBuilder = null;
 
             string respId = OrderOverView.OperatorID.ToString();
             string respText = txtOrderDeclineReason.Text;
             string respTime = timeLogNow.ToString();
 
-            Order.OrderDeclineReason.AppendLine($"-{respId}- {respTime} : {respText}");
+            Order.OrderDeclineReason = $"-{respId}- {respTime} : {respText}";
 
-            //orderDeclineBuilder.AppendLine($"-{respId}- {respTime} : {respText}");
+            Service.UpdateOrderAsync(Order);
 
-            //Order.OrderDeclineReason = orderDeclineBuilder.ToString();
-
-            //Det bortkommenterade är om man hellre vill lösa det med en string
-
-
-            //Add string OrderDeclineReason to DB
-            //Update DB with new OrderDeclineReason
-            //Link for stringbuilder in DB: 
-            //       https://www.tutorialsteacher.com/csharp/csharp-stringbuilder
+            
         }
-
-
-
     }
 }
